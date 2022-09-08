4f1c612b6465282b811ba2ff48d12885b0da6c95
SaguaroMaster <krisi889@gmail.com>
2022-09-08 10:12:13 +0200

    c

--- flashTest.py
+++ flashTest.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from gpiozero import Button as btn
+import signal                   
+import sys
+import RPi.GPIO as GPIO
 
-button = btn(17)
-button.when_pressed = print("Flash!")
+BUTTON_GPIO = 17
 
-
-while True:
-    pass
+def signal_handler(sig, frame):
+    GPIO.cleanup()
+    sys.exit(0)
+def button_callback(channel):
+    if not GPIO.input(BUTTON_GPIO):
+        print("Button pressed!")
+    else:
+        print("Button released!")
+if __name__ == '__main__':
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    
+    GPIO.add_event_detect(BUTTON_GPIO, GPIO.BOTH, 
+            callback=button_callback, bouncetime=50)
+    
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.pause()

8cca26556e6815be777d55565cf7fbee8a872876
SaguaroMaster <krisi889@gmail.com>
2022-09-09 21:35:21 +0200

    c

--- web/web.py
+++ web/web.py
@@ -7,6 +7,8 @@ from matplotlib.figure import Figure
 import io
 import threading
 import pandas
+import dateutil.relativedelta
+import calendar
 
 from flask import Flask, render_template, send_from_directory, make_response, request
 app = Flask(__name__)
@@ -37,7 +39,7 @@ def getFirstData():
 
 
 def getHistDataPower (numSamples1, numSamples2):
-	curs.execute("SELECT * FROM data WHERE timestamp >= '" + str(numSamples1) + "' AND timestamp <= '" + str(numSamples2) + "' ORDER BY timestamp DESC")
+	curs.execute("SELECT * FROM data WHERE timestamp >= '" + str(numSamples2 - timedelta(days=1)) + "' AND timestamp <= '" + str(numSamples2) + "' ORDER BY timestamp DESC")
 	data = curs.fetchall()
 	dates = []
 	power = []
@@ -49,14 +51,52 @@ def getHistDataPower (numSamples1, numSamples2):
 def getHistDataEnergy (numSamples1, numSamples2):
 	datesSum = []
 	energySum = []
-	timeInterval = pandas.date_range(str(numSamples1)[:10],str(numSamples2)[:10],freq='d').tolist()
+	timeInterval = pandas.date_range(str(numSamples2 - timedelta(days=30))[:10],str(numSamples2)[:10],freq='d').tolist()
 	for entry1 in timeInterval[:len(timeInterval)-1]:
 		entry2 = entry1 + timedelta(days=1)
 		curs.execute("SELECT SUM(energy) FROM data WHERE timestamp >= '" + str(entry1) + "' AND timestamp <= '" + str(entry2) + "'")
 		dataSum = curs.fetchall()
 		datesSum.append(str(entry1))
 		energySum.append(dataSum[0][0])
+	
+	energySum = [0 if v is None else v for v in energySum]
+	energySum = convertEnergyToKwh(energySum)
 	return datesSum, energySum
+
+def getHistDataEnergyDailyAvg (numSamples1, numSamples2):
+	datesSum = []
+	energySum = []
+
+	timeInterval = pandas.date_range(str(numSamples2 - timedelta(days=365))[:10],str(numSamples2)[:10],freq='M').tolist()
+	for entry1 in timeInterval[:len(timeInterval)]:
+		entry2 = entry1 + dateutil.relativedelta.relativedelta(months=1)
+		curs.execute("SELECT SUM(energy) FROM data WHERE timestamp >= '" + str(entry1) + "' AND timestamp <= '" + str(entry2) + "'")
+		dataSum = curs.fetchall()
+		daysInMonth = int(calendar.monthrange(entry2.year, entry2.month)[1])
+		datesSum.append(str(entry2))
+		energySum.append([0 if dataSum[0][0] is None else dataSum[0][0]][0]/daysInMonth)
+
+	energySum = [0 if v is None else v for v in energySum]
+	energySum = convertEnergyToKwh(energySum)
+	return datesSum, energySum
+
+def getHistDataEnergyMonthly (numSamples1, numSamples2):
+	datesSum = []
+	energySum = []
+	timeInterval = pandas.date_range(str(numSamples2 - timedelta(days=365))[:10],str(numSamples2)[:10],freq='M').tolist()
+	for entry1 in timeInterval[:len(timeInterval)]:
+		entry2 = entry1 + dateutil.relativedelta.relativedelta(months=1)
+		curs.execute("SELECT SUM(energy) FROM data WHERE timestamp >= '" + str(entry1) + "' AND timestamp <= '" + str(entry2) + "'")
+		dataSum = curs.fetchall()
+		datesSum.append(str(entry2))
+		energySum.append(dataSum[0][0])
+	energySum = [0 if v is None else v for v in energySum]
+	energySum = convertEnergyToKwh(energySum)
+	return datesSum, energySum
+
+def convertEnergyToKwh(energy):
+	energy = [v/1000 for v in energy]
+	return energy
 
 def setGlobalVars():
 	global numSamples1, numSamples2
@@ -102,11 +142,14 @@ def my_form_post():
 
     numSamples1 = request.form['numSamples1']
     numSamples2 = request.form['numSamples2']
+
+    numSamples1 = datetime.strptime(numSamples1, "%Y-%m-%d")
+    numSamples2 = datetime.strptime(numSamples2, "%Y-%m-%d")
     
     numSamples1_disp = str(numSamples1)[:10]
     numSamples2_disp = str(numSamples2)[:10]
 
-    numSamples2 = str(datetime.strptime(numSamples2, "%Y-%m-%d") + timedelta(days=1))
+    numSamples2 = numSamples2 + timedelta(days=1)
 
     lastDate, power, energy = getLastData()
     firstDate, nada1, nada2 = getFirstData()
@@ -138,7 +181,7 @@ def plot_power():
 		ys = power
 		fig = Figure()
 		axis = fig.add_subplot(1, 1, 1)
-		axis.set_title("Power [kW]")
+		axis.set_title("Power [kW] [1 Day]")
 		axis.set_xlabel("Date[M:D H:M:S]")
 		axis.set_xticks([0, int(len(ys)/2), int(len(ys)/1.1)])
 		axis.grid(True)
@@ -163,9 +206,57 @@ def plot_energy():
 		ys = energy
 		fig = Figure()
 		axis = fig.add_subplot(1, 1, 1)
-		axis.set_title("Energy / day [kWh]")
-		axis.set_xlabel("Date[Month : Day")
+		axis.set_title("Energy / day [kWh]  [30 Days]")
+		axis.set_xlabel("Date [Month : Day] ")
 		axis.set_xticks([0, int(len(ys)/2), int(len(ys)/1.1)])
+		axis.grid(True)
+		axis.bar(xs, ys, width=0.5)
+		canvas = FigureCanvas(fig)
+		output = io.BytesIO()
+		canvas.print_png(output)
+		response = make_response(output.getvalue())
+		response.mimetype = 'image/png'
+		return response
+	finally:
+		lock.release()
+
+@app.route('/plot/energydailyavg')
+def plot_energyDailyAvg():
+	try:
+		lock.acquire(True)
+		times, energy = getHistDataEnergyDailyAvg(numSamples1, numSamples2)
+		for j in range(len(times)):
+			times[j]=times[j][5:7]
+		xs = times
+		ys = energy
+		fig = Figure()
+		axis = fig.add_subplot(1, 1, 1)
+		axis.set_title("Average Energy / Day / Month [kWh]  [12 Months]")
+		axis.set_xlabel("Date [Month]")
+		axis.grid(True)
+		axis.bar(xs, ys, width=0.5)
+		canvas = FigureCanvas(fig)
+		output = io.BytesIO()
+		canvas.print_png(output)
+		response = make_response(output.getvalue())
+		response.mimetype = 'image/png'
+		return response
+	finally:
+		lock.release()
+
+@app.route('/plot/energymonthly')
+def plot_energyDailyMonthly():
+	try:
+		lock.acquire(True)
+		times, energy = getHistDataEnergyMonthly(numSamples1, numSamples2)
+		for j in range(len(times)):
+			times[j]=times[j][5:7]
+		xs = times
+		ys = energy
+		fig = Figure()
+		axis = fig.add_subplot(1, 1, 1)
+		axis.set_title("Energy / month [kWh] [12 Months]")
+		axis.set_xlabel("Date [Year : Month]")
 		axis.grid(True)
 		axis.bar(xs, ys, width=0.5)
 		canvas = FigureCanvas(fig)

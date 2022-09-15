694812aa462bfe0169e8e5a43ff5e631e0764692
SaguaroMaster <krisi889@gmail.com>
2022-09-15 11:11:14 +0200

    c

--- logDataBeta.py
+++ logDataBeta.py
@@ -19,17 +19,9 @@ GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def flashCounter(self):
 	global flashCount, time1
-
 	print("Light!")
 	flashCount = flashCount + 1
-	if time.time() > time1+sampleFreqency:
-		print(time.time)
-		energy = flashCount #Wh
-		power = energy * 0.36/(sampleFreqency/10) # kW
-		print("Power: " + str(power) + "kW, Energy: " + str(energy) + "Wh")
-		#logData(power, energy)
-		flashCount = 0
-		time1 = time.time()
+	
 
 GPIO.add_event_detect(17, GPIO.FALLING, callback=flashCounter, bouncetime=50)
 # log sensor data on database
@@ -43,4 +35,12 @@ def logData (power, energy):
 	conn.close()
 
 while True:
-	time.sleep(0.1)
+	if time.time() > time1+sampleFreqency:
+		print(time.time)
+		energy = flashCount #Wh
+		power = energy * 0.36/(sampleFreqency/10) # kW
+		print("Power: " + str(power) + "kW, Energy: " + str(energy) + "Wh")
+		#logData(power, energy)
+		flashCount = 0
+		time1 = time.time()
+	time.sleep(0.01)
